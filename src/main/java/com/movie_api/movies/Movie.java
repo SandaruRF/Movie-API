7bc4d414702538f1cb7828fe67a8d9cda73e7325
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-02-05 01:18:38 +0530

    Minor changes

--- src/main/java/com/movie_api/movies/Movie.java
+++ src/main/java/com/movie_api/movies/Movie.java
@@ -1,5 +1,6 @@
 package com.movie_api.movies;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +17,34 @@ import java.util.List;
 @NoArgsConstructor
 public class Movie {
     @Id
+    @JsonProperty
     private ObjectId id;
+
+    @JsonProperty("imdbId")  // Ensures JSON serialization
     private String imdbId;
+
+    @JsonProperty("title")
     private String title;
+
+    @JsonProperty("releaseDate")
     private String releaseDate;
+
+    @JsonProperty("trailerLink")
     private String trailerLink;
+
+    @JsonProperty("poster")
     private String poster;
+
+    @JsonProperty("genres")
     private List<String> genres;
+
+    @JsonProperty("backdrops")
     private List<String> backdrops;
+
     @DocumentReference
+    @JsonProperty
     private List<Review> reviewIds;
 }
+
+
+
